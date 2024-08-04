c90540fcb5f24601ef1fde3979a535028d020721
Biju Joseph <biju1984@gmail.com>
2024-08-04 10:56:59 +0400

    Chore: folder naming convention

--- api-composer/app/main.py
+++ api-composer/app/main.py
@@ -4,21 +4,35 @@ from app.api.routes import router
 from app.core.config import settings
 from app.core.logging_config import setup_logging
 from app.healthcheck import perform_health_checks
-
+from fastapi.middleware.cors import CORSMiddleware
+import debugpy
 
 def create_app() -> FastAPI:
     app = FastAPI(
         title=settings.PROJECT_NAME,
         version=settings.VERSION
     )
-    app.include_router(router)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router, prefix="/api")  # Ensure prefix matches frontend request path
+    
     @app.on_event("startup")
     async def startup_event():
         await perform_health_checks()
+    
     setup_logging()
     return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
+    debugpy.listen(("0.0.0.0", 5678))
+    print("Debugger is active")
+    uvicorn.run(app, host=settings.HOST, port=settings.PORT, reload=settings.RELOAD)

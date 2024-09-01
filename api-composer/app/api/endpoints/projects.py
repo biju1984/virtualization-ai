eeac3bcc96222f644157017aade1deabb904806a
Biju Joseph Tharakan <biju1984@gmail.com>
2024-09-01 22:20:12 +0400

    Feat: Add project create, fetch

--- api-composer/app/api/endpoints/projects.py
+++ api-composer/app/api/endpoints/projects.py
@@ -3,34 +3,36 @@ from sqlalchemy.orm import Session
 from app.api.schemas.project import ProjectCreate, ProjectResponse
 from app.api.services.project_service import create_project, get_projects, get_project
 from app.api.services.user_service import get_current_user
-from app.db.database import get_db
+from app.models.database import get_db
 from typing import List
 
 router = APIRouter()
 
+
 @router.post("/", response_model=ProjectResponse)
 def create_new_project(
-    project: ProjectCreate, 
+    project: ProjectCreate,
     db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
+    current_user=Depends(get_current_user)
 ):
     return create_project(db, project, current_user)
+
 
 @router.get("/", response_model=List[ProjectResponse])
 def read_projects(
     db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
+    current_user=Depends(get_current_user)
 ):
     return get_projects(db, current_user)
+
 
 @router.get("/{project_id}", response_model=ProjectResponse)
 def read_project(
     project_id: int,
     db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
+    current_user=Depends(get_current_user)
 ):
     db_project = get_project(db, project_id, current_user)
     if db_project is None:
         raise HTTPException(status_code=404, detail="Project not found")
     return db_project
-
--- api-composer/app/routes/index.py
+++ api-composer/app/routes/index.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.api.endpoints.nlp import router as nlp_router
-# from app.api.endpoints.projects import router as project_router
+from app.api.endpoints.projects import router as project_router
 from app.api.endpoints.users import router as user_router
 
 # Create a master router
@@ -8,5 +8,5 @@ router = APIRouter()
 
 # Include individual routers with their prefixes
 router.include_router(nlp_router, prefix="/nlp")
-# router.include_router(project_router, prefix="/projects")
+router.include_router(project_router, prefix="/projects")
 router.include_router(user_router, prefix="/user")
